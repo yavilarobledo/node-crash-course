4a0ad4f64c0b2fc4cb643d607cdaee9021e4bd7c
yavilar <yavilarobledo@ivytech.edu>
2023-12-17 22:58:02 -0500

    videos 7 and 8

--- app.js
+++ app.js
@@ -1,32 +1,56 @@
 const express = require('express');
+const morgan = require('morgan');
 
-//express app
+// express app
 const app = express();
 
-
-//listen for requests
+// listen for requests
 app.listen(3000);
 
+// register view engine
+app.set('view engine', 'ejs');
 
-app.get('/',(req,res)=>{
+// middleware & static files
+app.use(express.static('public'));
 
-    // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html',{root:__dirname});
+app.use((req, res, next) => {
+  console.log('new request made:');
+  console.log('host: ', req.hostname);
+  console.log('path: ', req.path);
+  console.log('method: ', req.method);
+  next();
 });
 
-app.get('/about',(req,res)=>{
-    res.sendFile('./views/index.html',{root:__dirname});
-    // res.send('<p>about page</p>');
+app.use((req, res, next) => {
+  console.log('in the next middleware');
+  next();
 });
 
-//redirects
-app.get('/about-us',(req,res)=>{
-    res.redirect('/about');
-})
+app.use(morgan('dev'));
 
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
+app.get('/', (req, res) => {
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
+});
 
-//404 page
-app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html',{root:__dirname})
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
 });
